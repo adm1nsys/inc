4cb759670c08b2595c902be5d2655ab23087ff0c
Admin inc <107643055+adm1nsys@users.noreply.github.com>
2024-02-18 22:10:17 +0200

    d

---
+++ data/corp copy.js
@@ -0,0 +1,14 @@
+{
+  "corporateDataArray": [
+    {
+      "name": "Admin inc.",
+      "logo_c": "a0",
+      "status": "Release_m",
+      "pcversion": "2.2PR",
+      "mobileversion": "2.2MR",
+      "vrversion": "2.2VRR",
+      "universalversion": "2.2UR",
+      "description": "description for Admin inc."
+    }
+  ]
+}
